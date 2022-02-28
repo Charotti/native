03d6faa25996e0f0da7b290bb897b12aefeaf920
unknown <clamy6@yahoo.fr>
2022-02-28 16:05:49 +0100

    views-text

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inner_container}>
+        <Text style={(styles.text, styles.firstText)}>hello</Text>
+        <Text style={(styles.text, styles.secondText)}>hello2</Text>
+        <Text style={(styles.text, styles.thirdText)}>hello3</Text>
+      </View>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "blue",
+
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  inner_container: {
+    backgroundColor: "white",
+    border: "1px solid blue",
+  },
+  text: {
+    marginBottom: 80,
+    marginTop: 80,
+    border: "1 px solid green",
+  },
+  firstText: {
+    fontSize: 30,
+  },
+  secondText: {
+    textAlign: "center",
+  },
+  thirdText: {
+    color: "grey",
   },
 });
